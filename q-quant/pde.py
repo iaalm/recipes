5421213e5f166c60a64b6d57eaa810c15b487798
iaalm <iaalmsimon@gmail.com>
2025-11-14 00:17:44 +0800

    Correct calc

--- q-quant/pde.py
+++ q-quant/pde.py
@@ -2,76 +2,66 @@
 This script used to calculate the price of a barrier option using the Black-Scholes model.
 Solved with Finite Difference Method (FDM) on Partial Differential Equation (PDE).
 """
-from QuantLib import *
+import QuantLib as ql
 import matplotlib.pyplot as plt
+import yfinance as yf
+
+# 获取英伟达（NVIDIA）的股票价格（2024/11/11 到 2025/11/11）
+start_date = "2024-11-11"
+end_date = "2025-11-11"
+nvda_data = yf.download("NVDA", start=start_date, end=end_date, auto_adjust=True)
+
+spot_price = nvda_data.iloc[0][('Close', "NVDA")]
+strike_price = spot_price
+barrier = strike_price * 1.45
 
 # 定义市场参数
 spot_price = 100.0
 strike_price = 100.0
 volatility = 0.4
 risk_free_rate = 0.05
-maturity = 0.01  # change to 0.01 is interesting
-barrier = 145.0
 barrier_payoff = 0.07
+exercise_date = ql.Date(11, 12, 2025)
 
 # 定义 QuantLib 对象
-spot = SimpleQuote(spot_price)
-q_rate = YieldTermStructureHandle(FlatForward(0, NullCalendar(), 0.0, Actual365Fixed()))
-r_rate = YieldTermStructureHandle(FlatForward(0, NullCalendar(), risk_free_rate, Actual365Fixed()))
-vol = BlackVolTermStructureHandle(BlackConstantVol(0, NullCalendar(), volatility, Actual365Fixed()))
+spot = ql.SimpleQuote(spot_price)
+q_rate = ql.YieldTermStructureHandle(ql.FlatForward(0, ql.NullCalendar(), 0.0, ql.Actual365Fixed()))
+r_rate = ql.YieldTermStructureHandle(ql.FlatForward(0, ql.NullCalendar(), risk_free_rate, ql.Actual365Fixed()))
+vol = ql.BlackVolTermStructureHandle(ql.BlackConstantVol(0, ql.NullCalendar(), volatility, ql.Actual365Fixed()))
 
 # Black-Scholes 模型
-process = BlackScholesMertonProcess(QuoteHandle(spot), q_rate, r_rate, vol)
+process = ql.BlackScholesMertonProcess(ql.QuoteHandle(spot), q_rate, r_rate, vol)
 
 # 构造障碍期权
-payoff = PlainVanillaPayoff(Option.Call, strike_price)
-exercise = EuropeanExercise(Date.todaysDate() + int(365 * maturity))
-option = BarrierOption(Barrier.UpOut, barrier, barrier_payoff, payoff, exercise)
+payoff = ql.PlainVanillaPayoff(ql.Option.Call, strike_price)
+exercise = ql.EuropeanExercise(exercise_date)
+option = ql.BarrierOption(ql.Barrier.UpOut, barrier, barrier_payoff, payoff, exercise)
 
 # 设置有限差分引擎
-engine = FdBlackScholesBarrierEngine(process, 100, 200, 0)
+engine = ql.FdBlackScholesBarrierEngine(process, 100, 200, 0)
 option.setPricingEngine(engine)
 
-# 计算期权价格
-price = option.NPV()
-print("Option Price:", option.NPV())
-print("Option Delta:", option.delta())
-print("Option Theta:", option.theta())
-print("Option Gamma:", option.gamma())
-
-
-spot_prices = [80 + 3 * i for i in range(22)]
-deltas = []
 prices = []
-thetas = []
-gammas = []
-
-for s in spot_prices:
-    spot.setValue(s)
+npvs = []
+deltas = []
+for idx, data in nvda_data.iterrows():
+    ql.Settings.instance().evaluationDate = ql.Date(data.name.day, data.name.month, data.name.year)
+    price = data[('Close', "NVDA")]
+    spot.setValue(price)
+    prices.append(price)
+    npv = option.NPV()
+    npvs.append(npv)
     delta = option.delta()
     deltas.append(delta)
-    price = option.NPV()
-    prices.append(price)
-    theta = option.theta()
-    thetas.append(theta)
-    gamma = option.gamma()
-    gammas.append(gamma)
 
-
-plt.subplot(4, 1, 1)
-plt.plot(spot_prices, deltas, label="Delta Curve")
+plt.subplot(3, 1, 1)
+plt.title("Price underlying asset")
+plt.plot(prices, label="Price")
+plt.subplot(3, 1, 2)
+plt.title("Price of Barrier Option")
+plt.plot(npvs, label="NPV")
+plt.subplot(3, 1, 3)
+plt.title("Delta Curve of Barrier Option")
+plt.plot(deltas, label="Delta")
 plt.legend()
-plt.grid()
-plt.subplot(4, 1, 2)
-plt.plot(spot_prices, prices, label="Price Curve")
-plt.legend()
-plt.grid()
-plt.subplot(4, 1, 3)
-plt.plot(spot_prices, thetas, label="Theta Curve")
-plt.legend()
-plt.grid()
-plt.subplot(4, 1, 4)
-plt.plot(spot_prices, gammas, label="Gamma Curve")
-plt.legend()
-plt.grid()
 plt.show()
